de3f24f30de95266456c7d0888252372257c368c
Oleksiy Protas <elfy.ua@gmail.com>
2016-09-16 04:24:58 +0300

    No gorilla dep, vanilla version

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
     "log"
     "net/http"
     "os"
-
-    "github.com/gorilla/mux"
 )
 
 func main() {
@@ -16,24 +14,10 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
-    router.HandleFunc("/todos", TodoIndex)
-    router.HandleFunc("/todos/{todoId}", TodoShow)
-
-    log.Fatal(http.ListenAndServe(":"+port, router))
+  http.HandleFunc("/issues", Issues);
+  log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome!")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Todo Index!")
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    todoId := vars["todoId"]
-    fmt.Fprintln(w, "Todo show:", todoId)
+func Issues(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome! "+r.UserAgent() )
 }
