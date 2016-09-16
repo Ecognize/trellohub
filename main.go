d0ca329ef597ea66431fe43a919554874dc6f326
Oleksiy Protas <elfy.ua@gmail.com>
2016-09-16 04:06:41 +0300

    Stolen from tutorial

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+func main() {
+  port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    router.HandleFunc("/todos", TodoIndex)
+    router.HandleFunc("/todos/{todoId}", TodoShow)
+
+    log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    todoId := vars["todoId"]
+    fmt.Fprintln(w, "Todo show:", todoId)
+}
