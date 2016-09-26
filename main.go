bec1cafe9af786476e90f20fbcf527a98bb16495
Oleksiy Protas <elfy.ua@gmail.com>
2016-09-26 23:27:15 +0300

    Trello class core

--- main.go
+++ main.go
@@ -8,20 +8,41 @@ import (
     "io/ioutil"
 )
 
+/* TODO separate file */
+type Trello struct {
+  BaseURL string
+  Token string
+  Key string
+} /* TODO make private */
+
+func NewTrello(key string, token string) *Trello {
+  t := new(Trello)
+  t.BaseURL = "https://api.trello.com/1"
+  t.Token = token
+  t.Key = key
+
+  return t
+}
+
+func (this *Trello) Get(rq string) string {
+  resp, err := http.Get(this.BaseURL + rq + "?key=" + this.Key + "&token=" + this.Token)
+  // TODO if error
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  err = err
+
+  return string(body[:])
+}
+
+
 func main() {
   /* Check if we are run to [re]-initialise the board */
   if (len(os.Args) >= 4) {
     // TODO make a proper class here
     key, token, boardid := os.Args[1], os.Args[2], os.Args[3]
-    baseurl := "https://api.trello.com/1"
+    trello := NewTrello(key, token)
 
-    resp, err := http.Get(baseurl + "/boards/" + boardid + "?key=" + key + "&token=" + token)
-    // TODO if error
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    err = err
-
-    fmt.Println(string(body[:]))
+    fmt.Println(trello.Get("/boards/" + boardid))
 
     /* Archive all lists */
 
